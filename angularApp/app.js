58dcd4b3e8d7476cf3e7d313b51fa97f77297ea6
Gwen Eskridge <dragonknightrose@gmail.com>
2015-11-29 21:25:28 -0600

    Added ability to log-in with dummy service Added ability to log-off Pages now re-direct to logon if user is not logged in

--- angularApp/app.js
+++ angularApp/app.js
@@ -16,8 +16,32 @@ angular.module('myApp', [
   'myApp.ptDemographics',
   'myApp.ptAuth',
   'myApp.version',
-  'filters'
+  'filters',
+  'AuthenticationService',
+  'ngCookies',
+  'base64'
 ]).
+
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/signIn'});
-}]);
+}]).
+
+run(['$rootScope', '$location', '$cookieStore', '$http', 'AuthenticationService', function($rootScope, $location, $cookieStore, $http, AuthenticationService){
+  $rootScope.globals = $cookieStore.get('globals') || {};
+  if($rootScope.globals.currentUser){
+    $http.defaults.headers.common['Authorization'] = 'Basic '+ $rootScope.globals.currentUser.auth;
+  }
+
+  $rootScope.$on('$locationChangeStart', function(event, next, current){
+    var restricted = $.inArray($location.path(), ['/signIn']) === -1;
+    var loggedIn = $rootScope.globals.currentUser;
+    if (restricted && !loggedIn) {
+      $location.path('/signIn')
+    };
+  })
+
+  $rootScope.signOut = function(){
+    AuthenticationService.ClearCredentials();
+    $location.path('/signIn');
+  }
+}])
--- angularApp/signIn/signIn.js
+++ angularApp/signIn/signIn.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp.signIn', ['ngRoute'])
-
+	
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/signIn', {
     templateUrl: 'signIn/signIn.html',
@@ -9,13 +9,27 @@ angular.module('myApp.signIn', ['ngRoute'])
   });
 }])
 
-.controller('SignInCtrl', ['$scope', function(sc) {
+.controller('SignInCtrl', ['$scope','$http', '$location',  'AuthenticationService', function(sc, $http, $location, AuthenticationService) {
 	//default username/pswd
 	sc.email = "testDoc@gmail.com";
 	sc.password = "password";
+	sc.invalidLogin = "false";
 
 	sc.signIn = function() {
-		console.log("Logged in")
+		console.log('Logging in');
+		AuthenticationService.Login(sc.email, sc.password, function(response){
+			if(response.success){
+				console.log('login success');
+				AuthenticationService.SetCredentials(sc.email, sc.password);
+				console.log(response);
+				if (response.type == 'physician') {
+					$location.path('/prov/dashboard').replace();
+				} else{$location.path('/pt/dashboard')}
+				
+			}
+			else{
+				console.log(response.message);
+				invalidLogin = true;}
+		});
 	}
-}]);
-
+}])
